3362b7d607fb5b9f2978d74eb4861ab04a5f8570
DarkFox1221 <62688366+DarkFox1221@users.noreply.github.com>
2021-12-15 13:52:21 +0300

    Add files via upload

---
+++ zadanie 2.4/zadanie 2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadanie_2._4_Practice_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 1 + i; j >= 1; j--)
+                {
+                    Console.Write(j + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
