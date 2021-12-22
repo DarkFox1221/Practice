f047e152c4563d54de17892ad62fe3f6d6c89f8f
DarkFox1221 <62688366+DarkFox1221@users.noreply.github.com>
2021-12-22 23:03:15 +0300

    Add files via upload

---
+++ zadanie11/zadanie_11/ConsoleApp11-с-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp11_с_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter pint coords x y:");
+                Point point = new Point(int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine()));
+
+                point.PrintPoint();
+
+                Console.Write("Multiply on scalar, enter scalar: ");
+                point.MultiplyPointOnScalar = int.Parse(Console.ReadLine());
+
+                point.PrintPoint();
+                
+                Console.Write("Enter two values to move point: ");
+                point.ReplacePoint(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+
+                point.PrintPoint();
+                Console.WriteLine($"Length: {point.CalcLengthToCoordinatesStart()}");
+            }
+            catch
+            {
+                Console.WriteLine("fail");
+            }
+        }
+    }
+}
