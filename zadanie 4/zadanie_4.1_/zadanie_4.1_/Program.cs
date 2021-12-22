a1f7450beb923135f57015475a2f234c63d3dcbc
DarkFox1221 <62688366+DarkFox1221@users.noreply.github.com>
2021-12-22 15:56:30 +0300

    Add files via upload

---
+++ zadanie 4/zadanie_4.1_/zadanie_4.1_/Program.cs
@@ -0,0 +1,16 @@
+﻿// Seeusing System; namespace Zadanie_4._1
+class Program
+{
+    static double F(int n)
+    {
+        if (n == 1) return -10;
+        else if (n == 2) return 2;
+        return F(Math.Abs((n - 1)) - (6 * n + 1));
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(F(1)); 
+        Console.WriteLine(F(2));
+    }
+}
+
