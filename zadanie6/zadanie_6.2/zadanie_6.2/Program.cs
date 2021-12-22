e218b7c1cfc7902d34915f104b878c3c66f080cd
DarkFox1221 <62688366+DarkFox1221@users.noreply.github.com>
2021-12-22 15:45:46 +0300

    Add files via upload

---
+++ zadanie6/zadanie_6.2/zadanie_6.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[100];
+            for (int i = 0; i < 100; i++)
+                arr[i] = rand.Next(-50, 51);
+
+
+            int min = arr.Min();
+            int max = arr.Max();
+
+            int min_index = Array.FindLastIndex(arr, delegate (int i)
+            {
+                return i == min;
+            });
+            int max_index = Array.FindIndex(arr, delegate (int i)
+            {
+                return i == max;
+            });
+
+            int sum = 0;
+
+            if (max_index <= min_index)
+            {
+                for (int i = max_index + 1; i < min_index; i++)
+                {
+                    sum += arr[i];
+                }
+                Console.WriteLine("Сумма элементов: {0}", sum);
+            }
+            else
+                Console.WriteLine("Максимальный результат расположен после минимального!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ zadanie6/zadanie_6.2wf/zadanie_6.2wf/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Zadanie_6._2_Graph_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сумма элементов";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 83);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Результат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(278, 180);
+            this.richTextBox1.Name = "textBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(223, 96);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "44";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
