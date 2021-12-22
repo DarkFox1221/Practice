12ec63414f6c072e58ece572d4dffbd1e8b72289
DarkFox1221 <62688366+DarkFox1221@users.noreply.github.com>
2021-12-22 15:43:29 +0300

    Add files via upload

---
+++ zadanie 4/zadanie_4.2/zadanie_4.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class tst
+{
+    static void FindFactors(int n, int i = 5)
+    {
+        if (n % i == 0)
+            Console.Write(i + " ");
+        if (i == n / 2) return;
+        FindFactors(n, ++i);
+    }
+    static void Main()
+    {
+        FindFactors(1000);
+        Console.WriteLine();
+    }
+}
