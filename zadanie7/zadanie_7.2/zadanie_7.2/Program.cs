342889a6b4e6d08c20d5a6220c7dc60a94724bf5
DarkFox1221 <62688366+DarkFox1221@users.noreply.github.com>
2021-12-22 15:49:54 +0300

    Add files via upload

---
+++ zadanie7/zadanie_7.2/zadanie_7.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение");
+            string[] vvod = Console.ReadLine().Split(' ', '.', ',', '!', '?');
+            int max = 0;
+            string slovo = "";
+            for (int i = 0; i < vvod.GetLength(0); i++)
+            {
+                if (vvod[i].Length > max)
+                {
+                    max = vvod[i].Length;
+                    slovo = vvod[i];
+                }
+            }
+            Console.WriteLine("Самое длинное слово: {0}", slovo);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ zadanie7/zadanie_7.2wf/zadanie_7.2wf/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_7._2_Graph_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = "Я хочу вечно цитировать сон";    
+            string[] words = text.Split(',', '.', ' ', '!', '?', ';', '-', '/', '*');   
+            string BiggestWord = words[0];     
+            int BiggestLength = words[0].Length;     
+            for (int i = 0; i < words.Length; i++)     
+            {
+                if (words[i] != "" && words[i].Length > BiggestLength)
+                {
+                    BiggestLength = words[i].Length;
+                    BiggestWord = words[i];
+                }
+            }
+            textBox1.Text = Convert.ToString(BiggestWord);
+        }
+    }
+}
+
+
